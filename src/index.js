c30928a37b075d328f026d3b45c160d50b602c28
Maha Magdy <mahamagdy3@gmail.com>
2021-09-14 19:46:46 +0200

    fetch the meals, and update the number of the meals in the header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import "bootswatch/dist/journal/bootstrap.min.css";
 import './css/style.css';
+
+async function egyptianMeals() {
+    let egyptianFood = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?a=Egyptian');
+    const egyptianMeals = await egyptianFood.json();
+    const egyptianMealsList = egyptianMeals.meals;
+    updateMealsNumber(egyptianMealsList);
+}
+
+window.addEventListener('load', egyptianMeals());
+
+function updateMealsNumber(mealsList) {
+    document.getElementById('meals-num').textContent = mealsList.length;
+}
