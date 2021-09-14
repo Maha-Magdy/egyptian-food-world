b3a26ec60b1fc7e3246e4231f2b5fae2f452512e
Maha Magdy <mahamagdy3@gmail.com>
2021-09-15 00:03:26 +0200

    Display number of likes for each item on the Homepage, and create feature: add  new like

--- src/index.js
+++ src/index.js
@@ -9,8 +9,12 @@ async function egyptianMeals() {
   );
   const egyptianMeals = await egyptianFood.json();
   const egyptianMealsList = egyptianMeals.meals;
+  let egyptianMealsLikes = await fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/necgfXW8nUf9YW13OlZY/likes'
+  );
+  const egyptianMealsLikesList = await egyptianMealsLikes.json();
   updateMealsNumber(egyptianMealsList);
-  createMealsObject(egyptianMealsList);
+  createMealsObject(egyptianMealsList, egyptianMealsLikesList);
 }
 
 window.addEventListener("load", egyptianMeals());
@@ -19,9 +23,16 @@ function updateMealsNumber(mealsList) {
   document.getElementById("meals-num").textContent = mealsList.length;
 }
 
-function createMealsObject(mealsList) {
+function createMealsObject(mealsList, likesList) {
   for (const meal of mealsList) {
-      const mealObj = new Meal(meal.strMeal, meal.strMealThumb, meal.idMeal);
+      let likes;
+      const likeObj = likesList.filter(obj => obj.item_id === meal.idMeal);
+      if (likeObj.length === 1) {
+        likes = likeObj[0].likes;
+      } else {
+        likes = 0;
+      }
+      const mealObj = new Meal(meal.strMeal, meal.strMealThumb, meal.idMeal, likes);
       mealObj.drawTheMeal();
   }
 }
